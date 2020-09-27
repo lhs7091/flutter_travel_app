9583eea9d4e7138a65f9c17654655837ff554ff8
lhs7091 <lhs7091@naver.com>
2020-09-27 10:36:15 +0900

    initial commit

---
+++ lib/export_path.dart
@@ -0,0 +1,11 @@
+// Screens(/lib/screen/*)
+export './screen/home_screen.dart';
+
+// model(/lib/model/*)
+export './model/activity_model.dart';
+export './model/destination_model.dart';
+export './model/hotel_model.dart';
+
+// widget(/lib/widget/*)
+export './widget/destination_carousel.dart';
+export './widget/hotel_carousel.dart';
